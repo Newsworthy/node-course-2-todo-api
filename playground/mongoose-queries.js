bebb19b24c561458beb7c1958518f7f41e378191
Nick Smith <development@newsworthyvision.com>
2018-11-13 19:22:38 +0000

    Add queries playground file

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -29,7 +29,7 @@ const {User} = require('./../server/models/user');
 //   console.log('Todo By Id', todo);
 // }).catch((e) => console.log(e));
 
-User.findById('57bdb0fcdedf88540bfa2d66').then((user) => {
+User.findById('5bdc6a33913ec4bf1ce26224').then((user) => {
   if (!user) {
     return console.log('Unable to find user');
   }
